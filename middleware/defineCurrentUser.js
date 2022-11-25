c84f5b2df2d7ce6c76602ac86225753dfb42b67b
unknown <kylelarsen12@gmail.com>
2022-11-25 17:13:46 -0500

    debugging branch for jwt

--- middleware/defineCurrentUser.js
+++ middleware/defineCurrentUser.js
@@ -4,20 +4,17 @@ const jwt = require("json-web-token");
 async function defineCurrentUser(req, res, next) {
   try {
     console.log(req.headers);
-    if (req.headers.authorization) {
-      const [method, token] = req.headers.authorization.split(" ");
-      if (method == "Bearer") {
-        const result = await jwt.decode(process.env.JWT_SECRET, token);
-        console.log(result);
-        const { id } = result.value;
-        let user = await Account.findOne({ _id: id });
-        req.currentUser = user;
-      }
+    const [method, token] = req.headers.authorization.split(" ");
+    if (method == "Bearer") {
+      const result = await jwt.decode(process.env.JWT_SECRET, token);
+      console.log(result);
+      const { id } = result.value;
+      let user = await Account.findOne({ _id: id });
+      req.currentUser = user;
     }
     next();
   } catch (err) {
     req.currentUser = null;
-    console.log(err);
     next();
   }
 }
