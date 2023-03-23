b2912cad52fe2462f10ada8b25ea92c24621c958
Nick D'Esposito <Nicholas D'Esposito>
2023-03-22 21:48:18 -0400

    added paypal routes

--- index.js
+++ index.js
@@ -1,65 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors')
+const cors = require("cors");
 const methodOverride = require("method-override");
 require("dotenv").config();
 const app = express();
 const fs = require("fs");
 const bodyParser = require("body-parser");
 const bcrypt = require("mongoose-bcrypt");
-const defineCurrentUser =require('./middleware/defineCurrentUser')
-const http = require('http')
-const { Server } = require('socket.io')
-const compression = require("compression")
+const defineCurrentUser = require("./middleware/defineCurrentUser");
+const http = require("http");
+const { Server } = require("socket.io");
+const compression = require("compression");
 
 //the code below came from: https://stackoverflow.com/questions/9177049/express-js-req-body-undefined?answertab=modifieddesc#tab-top
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(compression())
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
 app.use(defineCurrentUser);
 
-
-/** create chat server via socket.io  
-const server = http.createServer(app)
-const io = new Server({server,
-  cors:{
-    origin:"http://localhost:3000/myjobs",
-    methods: ["GET", "POST"]
-  }
-})
-
-//listen for socket connection
-
-io.on('connection', (socket)=>{
-  console.log(`User Connected: ${socket.id}`)
-
-  //listen for users joining
-  socket.on('join_room', (data)=>{
-    socket.join(data)
-    console.log(`User with socket id: ${socket.id} joined room ${data}`)
-  })
-
-  //listen for messages sent
-  socket.on('send_message', (data)=>{
-    socket.to(data.room).emit('receive_message', data)
-  })
-
-  //listen for disconnect
-  socket.on('disconnect', ()=>{
-    console.log(`User socket ${socket.id} has disconnected`)
-  })
-
-})
-
-//end of webchat socket integration 
-*/
-
 app.get("/", (req, res) => {
-  res.send("running");
+    res.send("running");
 });
 
 const memberAccount_controller = require("./controllers/memberAccount_controller");
@@ -68,22 +32,41 @@ app.use("/memberAccounts", memberAccount_controller);
 const job_controller = require("./controllers/job_controller");
 app.use("/jobs", job_controller);
 
-app.use('/chats', require('./controllers/chat_controller'))
+app.use("/chats", require("./controllers/chat_controller"));
 
-const search_controller = require("./controllers/search_controller")
-app.use("/search", search_controller)
+const search_controller = require("./controllers/search_controller");
+app.use("/search", search_controller);
 
 // const authentication_controller = require("./controllers/authentication");
 // app.use("/authentication", authentication_controller);
 
+app.post("/my-server/create-paypal-order", async (req, res) => {
+    try {
+        const order = await paypal.createOrder();
+        res.json(order);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+app.post("/my-server/capture-paypal-order", async (req, res) => {
+    const { orderID } = req.body;
+    try {
+        const captureData = await paypal.capturePayment(orderID);
+        res.json(captureData);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
 app.listen(process.env.PORT, () => {
-  console.log(`connected at port ${process.env.PORT}`);
+    console.log(`connected at port ${process.env.PORT}`);
 });
 
 mongoose.connect(
-  process.env.MONGO_URI,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => {
-    console.log("connected to mongo");
-  }
+    process.env.MONGO_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+        console.log("connected to mongo");
+    }
 );
